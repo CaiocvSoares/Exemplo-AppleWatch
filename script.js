3ab8938592896aca572873f257a0ffa1d019fcf8
Caio Caldas <caiocaldasvsoares@gmail.com>
2025-11-07 16:31:48 -0300

    Alterar Tamanho

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 const imagemVisualizacao = document.querySelector("#visualizacao img");
+const tituloProduto = document.querySelector("h1");
+
+const opcoesTamanho = ["41mm", "45mm"];
 
 let numImagemSelecionada = 1;
+let numTamanhoSelecionado = 1;
 
 function atualizarImagemSelecionada(){
     const opcaoImagemSelecionada = document.querySelector('[name = "opcao-imagem"]:checked').id.charAt(0);
     numImagemSelecionada = opcaoImagemSelecionada;
     imagemVisualizacao.src = "./imagens/opcoes-cores/imagens-azul-inverno/imagem-"+ numImagemSelecionada +".jpeg";
 }
+
+function atualizarTamanho (){
+    const opcaoTamanhoSelecionado = document.querySelector('[name = "opcao-tamanho"]:checked').id.charAt(0);
+    numTamanhoSelecionado = opcaoTamanhoSelecionado;
+
+    const tamanhoCaixa = opcoesTamanho [numTamanhoSelecionado];
+
+    tituloProduto.innerText = ("Pulseira loop esportiva azul-inverno para caixa de "+ tamanhoCaixa);
+    if (tamanhoCaixa === '41mm'){
+        imagemVisualizacao.classList.add("caixa-pequena");
+    }
+    else {
+        imagemVisualizacao.classList.remove("caixa-pequena");
+    }
+}
+
